27c69d8ba364143dcf4a121d731be49be4bdff0b
maritzée <amby.121192@gmail.com>
2023-10-10 16:30:12 -0600

    ⚡️ Update queries to create views and create notebook

--- src/data_cube.sql
+++ src/data_cube.sql
@@ -1,4 +1,5 @@
--- Cuboide base 4D
+-- Cuboide base 3D
+create view view_3D_nys as
 select
 	name as albatros_id,
 	substring(date, 1, 4)  as año,
@@ -10,6 +11,7 @@ group by albatros_id, año, temporada
 
 -- Cuboides 2D
 -- ID, año, all temporadas
+create view view_2D_110 as
 select 
 	name as albatros_id,
 	substring(date, 1, 4) as año,
@@ -22,6 +24,7 @@ group by albatros_id, año
 order by ConvexHullArea_ha2 desc;
 
 -- ID, all año,temporadas
+create view view_2D_101 as
 select 
 	name as albatros_id,
 	'ALL' as año,
@@ -32,30 +35,19 @@ from albatros_seasons as2
 group by albatros_id, temporada;
 
 
--- all ID, año, all posición, temporadas
+-- all ID, año, temporadas
+create view view_2D_011 as
 select 
 	'ALL' as albatros_id,
 	substring(date, 1,4) as año,
-	'ALL' as posición,
 	season as temporada,
 	st_convexhull(st_collect(geom)),
 	st_area(st_convexhull(st_collect(geom))::geography)/10000 as ConvexHullArea_ha2
 from albatros_seasons as2 
 group by año, season;
 
--- all ID, all año, posición, temporadas
-select 
-	'ALL' as albatros_id,
-	'ALL' as año,
-	season as temporada,
-	st_convexhull(st_collect(geom)),
-	st_area(st_convexhull(st_collect(geom))::geography)/10000 as ConvexHullArea_ha2
-from albatros_seasons as2 
-group by posición, temporada
-order by temporada;
-
 -- Cuboide 1D
-
+create view view_1D_001 as
 select 
 	'ALL' as id,
 	'ALL' as año,
@@ -65,6 +57,7 @@ select
 from albatros_seasons as2 
 group by temporada;
 
+create view view_1D_010 as
 select 
 	'ALL' as id,
 	substring(date, 1, 4)  as año,
@@ -74,6 +67,7 @@ select
 from albatros_seasons as2 
 group by año;
 
+create view view_1D_100 as
 select 
 	name as id,
 	'ALL' as año,
